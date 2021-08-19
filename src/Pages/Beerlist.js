fbc5d88c6b7baafd84247d401516df9f2133436a
goldmembrane <byenghyunhwang@Byengui-MacBook-Pro.local>
2021-08-19 21:48:07 +0900

    create Beerlist.js file at Page and button for moving to Beerlist.js

---
+++ src/Pages/Beerlist.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Beerlist = () => {
+    return (
+        <div>
+            Beerlist
+        </div>
+    )
+}
+
+export default Beerlist
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const Home = () => {
+const Home = (props) => {
   return (
     <div>
-      
+      Home
+      <button onClick = {() => props.history.push('/beerlist')}>맥주리스트로 이동</button>
     </div>
   )
 }
